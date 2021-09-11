829a8b01c7063c1e6254dbd38c49660b2dce3a22
marzuk16 <marzuk777@gmail.com>
2021-09-11 23:19:08 +0600

    toaster and messages authorize

--- controllers/inboxController.js
+++ controllers/inboxController.js
@@ -2,6 +2,7 @@ const Profile = require("../models/Profile");
 const Course = require("../models/Course");
 const Conversation = require("../models/Conversation");
 
+const escape = require("../utils/escape");
 
 const opOverLoad = (a, b) => {
 
@@ -30,7 +31,7 @@ exports.inboxGetController = async (req, res, next) => {
             console.log("conversationList: ", res.locals.conversationList);
 
             // emit socket event
-            global.io.emit("update_conversation", res.locals.conversationList);
+            // global.io.emit("update_conversation", res.locals.conversationList);
                 
             res.render("pages/inbox/inbox", {
                 title: "Inbox",
@@ -47,13 +48,39 @@ exports.inboxGetController = async (req, res, next) => {
 exports.getMessages = async (req, res, next) => {
 
     try {
+
+      //identify user is authorize or not
+      const course = await Course.findOne({
+          $and: [
+            { _id: req.params.conversation_id },
+            {
+              $or: [
+                { author: req.user._id },
+                { joinedStudent: { "$in": [req.user._id] } }
+              ]
+            }
+          ]
+      });
+
+      if( !course ){
+
+        return res.status(401).json({
+          errors: {
+            common: {
+              msg: "You are unauthorized user for this conversation!",
+            },
+          },
+        });
+      }
+
       const messages = await Conversation.find({
         course: req.params.conversation_id,
       }).sort("-createdAt");
   
-      const { author, joinedStudent } = await Course.findById(
+      /* const { author, joinedStudent } = await Course.findById(
         req.params.conversation_id
-      );
+      ); */
+      const { author, joinedStudent } = course;
   
       res.status(200).json({
         data: {
@@ -63,6 +90,7 @@ exports.getMessages = async (req, res, next) => {
         user: req.user.userId,
         conversation_id: req.params.conversation_id,
       });
+
     } catch (err) {
       res.status(500).json({
         errors: {
@@ -80,6 +108,31 @@ exports.sendMessagePostController = async (req, res, next) => {
     if (req.body.message){
 
       try {
+
+          //identify user is authorize or not
+          const course = await Course.findOne({
+            $and: [
+              { _id: req.body.conversationId },
+              {
+                $or: [
+                  { author: req.user._id },
+                  { joinedStudent: { "$in": [req.user._id] } }
+                ]
+              }
+            ]
+        });
+
+        console.log("course: ", course);
+        if( !course ){
+
+          return res.status(401).json({
+            errors: {
+              common: {
+                msg: "You are unauthorized user for this conversation!",
+              },
+            },
+          });
+        }
 
         // save message text in database
         const newMessage = new Conversation({
@@ -134,31 +187,54 @@ exports.sendMessagePostController = async (req, res, next) => {
 // search conversation
 exports.searchConversation = async (req, res, next) => {
 
-  const conversationSearch = req.body.conversation;
-  // console.log("search string: ", conversationSearch);
-  try {
-    if (conversationSearch !== "") {
+  const logedUser = req.user._id;
+  const searchQuery = req.body.conversation;
 
-      const conversationList = await Course.find(
+  const search_regex = new RegExp("^" + escape(searchQuery), "i");
+  
+  try {
+
+    if (searchQuery !== "") {
+
+      const conversations = await Course.find(
         {
           $or: [
             {
-              title: conversationSearch,
+              title: search_regex,
             },
             {
-              batch: conversationSearch,
+              batch: search_regex,
             },
             {
-              term: conversationSearch,
+              term: search_regex,
             },
           ],
         }
       );
-
+      
+      console.log("conversations: ", conversations);
+      let conversationList = [];
+      conversations.forEach( item => {
+        
+        item.joinedStudent.push(item.author);
+        if(item.joinedStudent.includes(logedUser))
+          conversationList.push(item);
+      });
+      
       res.json(conversationList);
 
     } else {
-      throw createError("You must provide some text to search!");
+      
+      let courses = await Profile.findOne({user: req.user._id})
+                                        .populate('course')
+                                        .populate('joinedClass')
+                                        .select({course: 1, _id: 0});
+            
+            // own created courses and joined courses
+            let conversationList = [...courses?.course, ...courses?.joinedClass];
+
+            res.json(conversationList);
+
     }
   } catch (err) {
     res.status(500).json({
